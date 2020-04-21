46caf7291d8a67f8a46297e0e3e554366a3c61da
Mobin <mobin.hj@gmail.com>
2020-04-21 10:28:55 +0430

    update Gradle and fragment

--- app/src/main/java/com/example/myapp05/MainActivity.kt
+++ app/src/main/java/com/example/myapp05/MainActivity.kt
@@ -1,79 +1,51 @@
 package com.example.myapp05
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.net.URL
 import java.util.*
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_city.*
 
 class MainActivity : AppCompatActivity() {
 
 
     val appCode = "xdjT6t-y6emD9tugkEW_7w"
     val api = "NGkPDa4koRes4s9mNW0s"
-    var city: String = ""
+    var city: String = "sari"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tabLayout = tabBar
+        val tabItemCity = tabCity
+        val tabItemMap = tabMap
+        val viewPager = viewPager
         update_at.text = Date().toString()
-        submitButton.setOnClickListener {
-            city = cityEditText.text.toString()
-            if (city != "") {
-                   weatherTask().execute()
-               }else {
-                Toast.makeText(this,"Please enter a City",Toast.LENGTH_SHORT).show()
+        val pagerAdapter = pagerAdapter(supportFragmentManager, tabLayout.tabCount)
+        viewPager.adapter = pagerAdapter
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager.currentItem = tab.position
+                }
             }
-        }
-    }
-    inner class weatherTask() : AsyncTask<String ,Void, String>(){
-        override fun onPreExecute() {
-            super.onPreExecute()
-            loader.visibility = View.INVISIBLE
-            mainContainer.visibility = View.VISIBLE
-            errorText.visibility = View.GONE
-        }
 
-        override fun doInBackground(vararg params: String?): String? {
-            return try {
-                URL("https://weather.cit.api.here.com/weather/1.0/report.json?product=observation&name=$city&oneobservation=true&app_id=$api&app_code=$appCode").readText(Charsets.UTF_8)
-            }catch (e:Exception){
-                null
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
             }
-        }
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            try {
-                val jsonObj = JSONObject(result)
-                val observations = jsonObj.getJSONObject("observations")
-                val location = observations.getJSONArray("location").getJSONObject(0)
-                val observation = location.getJSONArray("observation").optJSONObject(0)
-                val city = observation.getString("city")
-                val temper = observation.getString("temperature")+"˚C"
-                val icone = observation.getString("iconLink")
-                val country =observation.getString("country")
-                val latitude = observation.getString("latitude")+" N"
-                val longitude = observation.getString("longitude")+" E"
-                Picasso.with(this@MainActivity).load(icone).into(imageSeason)
 
-                latTExtView.text = latitude
-                longTextView.text = longitude
-
-                temp.text = temper
-                address.text = "$city,$country"
-
-                loader.visibility = View.INVISIBLE
-                mainContainer.visibility = View.VISIBLE
-            } catch (e: Exception) {
-               loader.visibility = View.GONE
-                errorText.visibility = View.VISIBLE
+            override fun onTabReselected(tab: TabLayout.Tab?) {
             }
-        }
+        })
     }
 }
+
+
+
+
 
 
