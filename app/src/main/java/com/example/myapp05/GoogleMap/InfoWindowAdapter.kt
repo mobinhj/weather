c5a0c060b4da7d09718f7a42804c3e3c9903a61d
Mobin <mobin.hj@gmail.com>
2020-06-02 18:52:27 +0430

    Create GoogleMap InfoWindowAdapter

---
+++ app/src/main/java/com/example/myapp05/GoogleMap/InfoWindowAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.myapp05.GoogleMap
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import com.example.myapp05.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+
+class CustomInfoWindowAdapter(context: Context):GoogleMap.InfoWindowAdapter {
+    val mWindow = (context as Activity).layoutInflater
+        .inflate(R.layout.marker_info,null)
+    private fun rendowWindowText(marker: Marker,view: View){
+        val title   = view.findViewById<TextView>(R.id.title)
+        val snippet = view.findViewById<TextView>(R.id.snippet)
+        title.text = marker.title
+        snippet.text = marker.snippet
+    }
+    override fun getInfoContents(marker: Marker): View {
+        rendowWindowText(marker,mWindow)
+        return mWindow
+    }
+    override fun getInfoWindow(marker: Marker): View {
+        rendowWindowText(marker,mWindow)
+        return mWindow
+    }
+}
